d6eb4353875e38589a74e6a2bdeebb2ce931c10e
Xola11 <72969548+Xola11@users.noreply.github.com>
2021-08-02 15:26:13 +0200

    Add files via upload

---
+++ CODE/CODE/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(1, 100));
+            Console.ReadLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                Console.WriteLine(i);
+
+                while (true)
+                {
+
+                    Random random = new Random();
+
+                    int correctNumber = random.Next(1, 100);
+                    int guess = 7;
+
+                    Console.WriteLine("Write a number");
+
+                    while (guess != correctNumber)
+                    {
+                        string input = Console.ReadLine();
+
+                        if (!int.TryParse(input, out guess))
+                        {
+
+                            Console.WriteLine("Please use an actual number");
+
+                            continue;
+                        }
+
+                        guess = Int32.Parse(input);
+
+                        if (guess != correctNumber)
+                        {
+
+                            Console.WriteLine(rand.Next(1, 100)); ;
+                        }
+                    }
+
+                    Console.WriteLine("CORRECT!! You guessed it!");
+
+                    Console.WriteLine("type a sentinel value [-1]");
+
+                    int answer = 7;
+
+                    if (answer == 7)
+                    {
+                        continue;
+                    }
+                    else if (answer == 7)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Message User Input/Message User Input/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User");
+
+            const int MaxLength = 140;
+            var message = " ";
+
+            do
+            {
+                Console.WriteLine("Please type out the message you wish to write for the service provider. The message must not exceed 140 characters: ");
+                message = Console.ReadLine();
+
+                if (message.Length <= MaxLength)
+                {
+                    Console.WriteLine("Messsage is sent");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You have exceeded the given amount of characters please try again! ");
+                }
+            } while (message.Length > MaxLength);
+        }
+    }
+}
